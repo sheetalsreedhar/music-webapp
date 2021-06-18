ab7fa249e18b65ad89ce69dac93f6dd7a106d062
sheetalsreedhar <sheebabuloo1@gmail.com>
2021-06-18 14:58:53 +0530

    master

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100:'#181A25',
+          200:'#1D1F2B',
+        },
+        secondary: {
+          100: '#FFFFFF',
+          200: '',
+        }
+    },
+    width:{
+      '100':'23%',
+      '105':'30%',
+      '110':'55%'
+    }
+      // fontFamily: {
+      //   body: ['Nunito']
+      // }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
